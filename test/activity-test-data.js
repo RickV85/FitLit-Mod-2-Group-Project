2d2826e1c84c06decc19fd8bbb9a63e9ee948034
Rick Vermeil <rickv85@gmail.com>
2023-01-03 14:53:13 -0700

    Add activity test data

---
+++ test/activity-test-data.js
@@ -0,0 +1,213 @@
+const activityTestData = [
+  {
+  userID: 20,
+  date: "2019/06/15",
+  numSteps: 9052,
+  minutesActive: 168,
+  flightsOfStairs: 14
+  },
+  {
+  userID: 20,
+  date: "2019/06/16",
+  numSteps: 6269,
+  minutesActive: 126,
+  flightsOfStairs: 32
+  },
+  {
+  userID: 20,
+  date: "2019/06/17",
+  numSteps: 3081,
+  minutesActive: 253,
+  flightsOfStairs: 44
+  },
+  {
+  userID: 20,
+  date: "2019/06/18",
+  numSteps: 7783,
+  minutesActive: 86,
+  flightsOfStairs: 11
+  },
+  {
+  userID: 20,
+  date: "2019/06/19",
+  numSteps: 14356,
+  minutesActive: 50,
+  flightsOfStairs: 26
+  },
+  {
+  userID: 20,
+  date: "2019/06/20",
+  numSteps: 8021,
+  minutesActive: 114,
+  flightsOfStairs: 47
+  },
+  {
+  userID: 20,
+  date: "2019/06/21",
+  numSteps: 13376,
+  minutesActive: 60,
+  flightsOfStairs: 3
+  },
+  {
+  userID: 20,
+  date: "2019/06/22",
+  numSteps: 13094,
+  minutesActive: 285,
+  flightsOfStairs: 4
+  },
+  {
+  userID: 20,
+  date: "2019/06/23",
+  numSteps: 5249,
+  minutesActive: 40,
+  flightsOfStairs: 26
+  },
+  {
+  userID: 20,
+  date: "2019/06/24",
+  numSteps: 2578,
+  minutesActive: 134,
+  flightsOfStairs: 6
+  },
+  {
+  userID: 21,
+  date: "2019/06/15",
+  numSteps: 3033,
+  minutesActive: 208,
+  flightsOfStairs: 45
+  }, 
+  {
+  userID: 21,
+  date: "2019/06/16",
+  numSteps: 8858,
+  minutesActive: 277,
+  flightsOfStairs: 12
+  },
+  {
+  userID: 21,
+  date: "2019/06/17",
+  numSteps: 12195,
+  minutesActive: 296,
+  flightsOfStairs: 24
+  },
+  {
+  userID: 21,
+  date: "2019/06/18",
+  numSteps: 8781,
+  minutesActive: 108,
+  flightsOfStairs: 34
+  },
+  {
+  userID: 21,
+  date: "2019/06/19",
+  numSteps: 7570,
+  minutesActive: 53,
+  flightsOfStairs: 37
+  },
+  {
+  userID: 21,
+  date: "2019/06/20",
+  numSteps: 6101,
+  minutesActive: 130,
+  flightsOfStairs: 43
+  },
+  {
+  userID: 21,
+  date: "2019/06/21",
+  numSteps: 13842,
+  minutesActive: 274,
+  flightsOfStairs: 11
+  },
+  {
+  userID: 21,
+  date: "2019/06/22",
+  numSteps: 5807,
+  minutesActive: 143,
+  flightsOfStairs: 26
+  },
+  {
+  userID: 21,
+  date: "2019/06/23",
+  numSteps: 13639,
+  minutesActive: 183,
+  flightsOfStairs: 25
+  },
+  {
+  userID: 21,
+  date: "2019/06/24",
+  numSteps: 3008,
+  minutesActive: 89,
+  flightsOfStairs: 17
+  },
+  {
+  userID: 22,
+  date: "2019/06/15",
+  numSteps: 7288,
+  minutesActive: 46,
+  flightsOfStairs: 28
+  },
+  {
+  userID: 22,
+  date: "2019/06/16",
+  numSteps: 12631,
+  minutesActive: 125,
+  flightsOfStairs: 49
+  },
+  {
+  userID: 22,
+  date: "2019/06/17",
+  numSteps: 3274,
+  minutesActive: 219,
+  flightsOfStairs: 45
+  },
+  {
+  userID: 22,
+  date: "2019/06/18",
+  numSteps: 2087,
+  minutesActive: 202,
+  flightsOfStairs: 1
+  },
+  {
+  userID: 22,
+  date: "2019/06/19",
+  numSteps: 8702,
+  minutesActive: 248,
+  flightsOfStairs: 23
+  },
+  {
+  userID: 22,
+  date: "2019/06/20",
+  numSteps: 4692,
+  minutesActive: 33,
+  flightsOfStairs: 42
+  },
+  {
+  userID: 22,
+  date: "2019/06/21",
+  numSteps: 4350,
+  minutesActive: 44,
+  flightsOfStairs: 12
+  },
+  {
+  userID: 22,
+  date: "2019/06/22",
+  numSteps: 4449,
+  minutesActive: 51,
+  flightsOfStairs: 19
+  },
+  {
+  userID: 22,
+  date: "2019/06/23",
+  numSteps: 9085,
+  minutesActive: 276,
+  flightsOfStairs: 19
+  },
+  {
+  userID: 22,
+  date: "2019/06/24",
+  numSteps: 5863,
+  minutesActive: 164,
+  flightsOfStairs: 8
+  }
+];
+module.exports = activityTestData;
