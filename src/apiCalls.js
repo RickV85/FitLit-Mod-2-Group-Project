f89bef8b95de9ace27e908c4a4ccea267665497a
Faye Rosenshein <rosefaye.a@gmail.com>
2023-01-05 12:37:14 -0700

    fix: Add one more change to the previous commit

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -40,28 +40,58 @@ function loadUserData() {
 function loadSleepData() {
     const sleepURL = 'http://localhost:3001/api/v1/sleep';
     return fetch(sleepURL)
-        .then((response) => response.json())
+    .then((response) => {
+        if (response.ok) {
+            return response.json()
+        }
+        throw Promise.reject(response)
+    })
         .then((data) => {
+            errorHandeling('message', 'noError')
             sleepData = data.sleepData;
             return sleepData;
+        })
+        .catch((response) => {
+            errorHandeling('Sorry, the server is down. Try again later', 'error')
+            console.log('Something went wrong: ', response);
         })
 }
 function loadHydrationData() {
     const hydrationURL = 'http://localhost:3001/api/v1/hydration';
     return fetch(hydrationURL)
-        .then((response) => response.json())
+    .then((response) => {
+        if (response.ok) {
+            return response.json()
+        }
+        throw Promise.reject(response)
+    })
         .then((data) => {
+            errorHandeling('message', 'noError')
             hydroData = data.hydrationData;
             return hydroData;
+        })
+        .catch((response) => {
+            errorHandeling('Sorry, the server is down. Try again later', 'error')
+            console.log('Something went wrong: ', response);
         })
 }
 function loadActivityData() {
     const userURL = 'http://localhost:3001/api/v1/activity';
     return fetch(userURL)
-        .then((response) => response.json())
+    .then((response) => {
+        if (response.ok) {
+            return response.json()
+        }
+        throw Promise.reject(response)
+    })
         .then((data) => {
+            errorHandeling('message', 'noError')
             activityData = data.activityData;
             return activityData;
+        })
+        .catch((response) => {
+            errorHandeling('Sorry, the server is down. Try again later', 'error')
+            console.log('Something went wrong: ', response);
         })
 }
 export default { loadUserData, loadSleepData, loadHydrationData, loadActivityData };
